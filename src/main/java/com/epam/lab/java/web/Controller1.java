2e70b156aa4dd07e9c79f07152b3fad02aac1a06
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2018-05-23 17:37:22 +0300

    http

---
+++ src/main/java/com/epam/lab/java/web/Controller1.java
@@ -0,0 +1,30 @@
+package com.epam.lab.java.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(urlPatterns = "/user")
+public class Controller1 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+//        response.setIntHeader("Content-Length", 100_500);
+        response.setCharacterEncoding(String.valueOf(StandardCharsets.UTF_8));
+        try (PrintWriter writer = response.getWriter()) {
+            writer.println(
+                    "<http><head><title>Мама мыла раму</title></head><body>Мама мыла раму!</body></http>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
